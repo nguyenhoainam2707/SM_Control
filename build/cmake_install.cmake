963c9af5cd5a78bb0eb66e5d4dab44835a27c038
nguyenhoainam2707 <nguyennamyp54@gmail.com>
2024-09-09 16:35:27 +0700

    add

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Pico/atSampling_Device/atSampling_Device
+# Install script for directory: D:/Github/SM_Control
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Pico/atSampling_Device/atSampling_Device/build/pico-sdk/cmake_install.cmake")
+  include("D:/Github/SM_Control/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Pico/atSampling_Device/atSampling_Device/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Github/SM_Control/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- build/pico-sdk/cmake_install.cmake
+++ build/pico-sdk/cmake_install.cmake
@@ -39,16 +39,16 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Pico/atSampling_Device/atSampling_Device/build/pico-sdk/tools/cmake_install.cmake")
+  include("D:/Github/SM_Control/build/pico-sdk/tools/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Pico/atSampling_Device/atSampling_Device/build/pico-sdk/src/cmake_install.cmake")
+  include("D:/Github/SM_Control/build/pico-sdk/src/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Pico/atSampling_Device/atSampling_Device/build/pico-sdk/docs/cmake_install.cmake")
+  include("D:/Github/SM_Control/build/pico-sdk/docs/cmake_install.cmake")
 endif()
 
